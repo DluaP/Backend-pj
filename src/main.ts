0ad07a4d777c5964ce1ae3a55f3143fceaf280d5
DluaP <g.143.vasco@gmail.com>
2023-02-10 20:19:18 +0700

    แก้ port

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
 const document = SwaggerModule.createDocument(app, config);
 SwaggerModule.setup('api', app, document);
   app.enableCors();
-  await app.listen(3100);
+  await app.listen(9997);
 }
 bootstrap();
